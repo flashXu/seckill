7dc5140cb5b6b68495eabbcbdc14ce821ef23abe
Wingshine <jssjh2008@163.com>
2017-03-19 04:55:53 +0800

    01

---
+++ src/main/java/org/seckill/dao/SeckillDao.java
@@ -0,0 +1,42 @@
+package org.seckill.dao;
+
+
+import java.util.Date;
+import java.util.List;
+
+import org.seckill.entity.Seckill;
+
+
+/**
+ * @author jssjh
+ *
+ */
+public interface SeckillDao
+{
+
+    /**
+     * 减库存
+     * @param seckillId
+     * @param killTime
+     * @return 如果影响行数>1，表示更新库存的记录行数
+     */
+    int reduceNumber(long seckillId, Date killTime);
+
+    /**
+     * 根据id查询秒杀的商品信息
+     * @param seckillId
+     * @return
+     */
+    Seckill queryById(long seckillId);
+
+    /**
+     * 根据偏移量查询秒杀商品列表
+     * @param offset
+     * @param limit
+     * @return
+     */
+    List<Seckill> queryAll(int offset,int limit);
+
+
+
+}
